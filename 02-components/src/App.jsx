f9a00da84be1ead8df7091a74315c5b7244f945d
Muhammad Hashir <contact.hmhashir@gmail.com>
2026-06-23 22:20:31 +0500

    component is done

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import carders from './components/card'
+import card from './components/card'
+import navbar from './components/navbar'
 
 const App = () => {
   return (
     <>
     <div>App</div>
-    {carders()}
+    {card()}
+    {navbar()}
     </>
   )
 }
--- 02-components/src/components/card.jsx
+++ 02-components/src/components/card.jsx
@@ -1,5 +1,14 @@
-function card(){
-    return 'Hi Im Card'
+import React from 'react'
+
+const card = () => {
+  return (
+    <>
+        <div className="card">
+        <h1>Muhammad Hashir</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam, modi? Quam magnam nulla soluta, ullam at deleniti tempore vel esse, laboriosam cum sequi nostrum quisquam molestias rerum adipisci fugiat aperiam.</p>
+        </div>
+    </>
+  )
 }
 
 export default card
