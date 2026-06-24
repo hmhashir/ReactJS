6e43badb74fc29f65b2a0a66980219cf962ffeb8
Muhammad Hashir <contact.hmhashir@gmail.com>
2026-06-24 22:18:11 +0500

    props done successfully

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Card from './assets/components/Card'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='parent'>
+      <Card user='Hashir' age={18} img='https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <Card user='Haris' age={28} img='https://images.unsplash.com/photo-1752801516481-cfb8c47ee9a3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <Card user='Hasham' age={24} img='https://images.unsplash.com/photo-1751554933476-d029737d58b2?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+    </div>
   )
 }
 
