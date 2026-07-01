c1886a0bf923c9abe305dab70ed7b9c69ec724f2
Muhammad Hashir <contact.hmhashir@gmail.com>
2026-07-01 16:54:37 +0500

    project card strucrure added

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='parent'>App</div>
+    <div className='parent'>
+      <div className="card">
+        <div className="top">
+          <img src="" alt="" />
+          <button>Save</button>
+        </div>
+      </div>
+    </div>
   )
 }
-
 export default App
